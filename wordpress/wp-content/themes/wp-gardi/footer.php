67fe641c26a007baaede676fdb5994a42402108f
crazyyy <crazyyy@gmail.com>
2017-05-15 01:39:51 +0300

    small fix

--- wordpress/wp-content/themes/wp-gardi/footer.php
+++ wordpress/wp-content/themes/wp-gardi/footer.php
@@ -5,22 +5,23 @@
                                 <a href="<?php echo home_url(); ?>" class="footer-link" itemprop="name" title="Гарди. Декоративные растения и цветы, саженцы"> Декоративные растения и цветы, саженцы</a>
                                 <p itemprop="description">Посадочный материал от производителя.
                                     <br> Только саженцы выращенные в Украинском климате.
-                                    <br> Реёнированные растения от питомника с 40-ка летним стажем.
+                                    <br> Районированные растения от питомника с 40-ка летним стажем.
                                 </p>
                             </div>
                             <div class="footer-column-small margin-right55">
                                 <a href="#" class="footer-link" title="Питомник декоративных растений">Питомник декоративных растений</a>
-                                <p itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">Винница, Украина</p>
-                                <p>Tел.: <span itemprop="telephone">+380 67 775 27 48</span></p>
+                                <p itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">с.Некрасово, Винница, Украина</p>
+                                <p>Садовые услуги: <span itemprop="telephone"><a class="header-phone--bott" href="tel:+380677752748">+380 67 775 27 48</a></span></p>
+                                <p>Приём заказов: <span itemprop="telephone"><a class="header-phone--bott" href="tel:+380679588104">+38 067 958 81 04</a> (Светлана)</span></p>
                                 <p>Email: <span itemprop="email">
-                                <a href="mailto:lopatinskii_sad@mail.ru" class="footer-email">lopatinskii_sad@mail.ru</a></span></p>
+                                <a href="mailto:ivansadss@gmail.com" target="_blank" class="footer-email">lopatinskii_sad@mail.ru</a></span></p>
                             </div>
                             <div class="footer-column-small">
                                 <a href="#" class="super-button recall footer-recall">Перезвоните мне</a>
                             </div>
                             <div class="border-separate"></div>
                             <div class="copyright-block">
-                                <p class="gardi-copyright">© 2016 Сад Лопатина. Питомник плодовых и декоративных деревьев и кустов в Винница. Декоративные растения и цветы, саженцы. Посадочный материал от производителя. Только саженцы выращенные в Украинском климате. Реёнированные растения от питомника с 40-ка летним стажем.</p>
+                                <p class="gardi-copyright">© 2016 Сад Лопатина. Питомник плодовых и декоративных деревьев и кустов в Винница. Декоративные растения и цветы, саженцы. Посадочный материал от производителя. Только саженцы выращенные в Украинском климате. Районированные растения от питомника с 40-ка летним стажем.</p>
                                 <div class="b-content__social-icons-wrapper margin-top10">
                                     <a href="https://vk.com/id137855179" target="_blank" rel="nofollow" class="b-social-link b-social-link_vk" title="Питомник растений в Вконтакте"></a>
 
@@ -37,6 +38,9 @@
         </div>
     </div>
     <?php wp_footer(); ?>
+
+	<script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
 </body>
 
 </html>
